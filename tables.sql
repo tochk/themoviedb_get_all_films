13cd1dfe04f227d459b0ccab9a0d2b250440d59f
Alexey Andreev <bezumny.tochk@gmail.com>
2016-04-14 15:41:42 +0300

    Fixed sql tables

--- tables.sql
+++ tables.sql
@@ -1,34 +1,62 @@
-CREATE TABLE `films` (
+CREATE TABLE `genres` (
   `id` int(11) NOT NULL,
-  `original_title` varchar(100) DEFAULT NULL,
-  `title` varchar(100) DEFAULT NULL,
-  `overview` text,
-  `video` text,
-  `backdrop_path` text,
-  `poster_path` text,
-  `release_date` text,
-  `original_language` varchar(10) DEFAULT NULL,
-  `adult` varchar(10) DEFAULT NULL
+  `name` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `genre_movies_merge` (
+  `id` int(11) NOT NULL,
+  `genreid` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `genre_series_merge` (
+  `id` int(11) NOT NULL,
+  `genreid` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `movies` (
+  `id` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `original_title` varchar(200) NOT NULL,
+  `overview` longtext NOT NULL,
+  `poster_path` varchar(200) DEFAULT NULL,
+  `release_date` varchar(50) NOT NULL,
+  `original_language` varchar(10) NOT NULL,
+  `popularity` float NOT NULL,
+  `vote_count` int(11) NOT NULL,
+  `vote_average` float NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
-
-CREATE TABLE `tv` (
+CREATE TABLE `series` (
   `id` int(11) NOT NULL,
-  `original_name` varchar(200) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `overview` text NOT NULL,
-  `first_air_date` text NOT NULL,
-  `backdrop_path` text,
-  `poster_path` text,
-  `original_language` varchar(100) NOT NULL,
-  `origin_country` varchar(100) NOT NULL
+  `title` varchar(100) NOT NULL,
+  `original_title` varchar(200) NOT NULL,
+  `overview` longtext NOT NULL,
+  `first_air_date` varchar(100) NOT NULL,
+  `poster_path` varchar(200) DEFAULT NULL,
+  `original_language` varchar(10) NOT NULL,
+  `popularity` float NOT NULL,
+  `vote_count` int(11) NOT NULL,
+  `vote_average` float NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
-ALTER TABLE `films`
+ALTER TABLE `genres`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `tv`
+ALTER TABLE `genre_movies_merge`
+  ADD PRIMARY KEY (`id`,`genreid`),
+  ADD KEY `genre_movies_merge_fk1` (`genreid`);
+
+ALTER TABLE `movies`
   ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `series`
+  ADD PRIMARY KEY (`id`);
+  
+ALTER TABLE `genre_series_merge`
+  ADD PRIMARY KEY (`id`,`genreid`),
+  ADD KEY `genre_series_merge_fk1` (`genreid`);
+
+
 
